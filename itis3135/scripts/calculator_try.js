1f021a2040113cb04c005405cccbebd188992846
msolom15 <supersaiyen809@gmail.com>
2021-10-01 22:22:27 -0400

    Create calculator_try.js

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,54 @@
+
+
+
+function C1(){
+    var number1 = 1;
+    return this;
+    
+}
+
+function C2(){
+    var number2 = 2;
+    return this;
+}
+
+function C3(){
+    var number3 = 3;
+    return this;
+}
+
+function C4(){
+    var number4 = 4;
+    return this;
+}
+
+function C5(){
+    var number5 = 5;
+    return this;
+}
+
+function C6(){
+    var number6 = 6;
+    return this;
+}
+
+function C7(){
+    var number7 = 7;
+    return this;
+}
+
+function C8(){
+    var number8 = 8;
+    return this;
+}
+
+function C9(){
+    var number9 = 9;
+    return this;
+    
+}
+
+function Equals(number1,number2){
+    var Final= number1*number2;
+    window.alert(Final);
+}
