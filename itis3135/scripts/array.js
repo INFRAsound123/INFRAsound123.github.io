4e4603d7447ef796f953d06a9465908bebf98c3e
msolom15 <supersaiyen809@gmail.com>
2021-10-08 23:46:53 -0400

    Create array.js

---
+++ itis3135/scripts/array.js
@@ -0,0 +1,22 @@
+var salaries = [];
+var person = [];
+
+
+function addSalary(){
+    
+}
+
+}
+
+function displayResults(){
+    
+}
+
+function displaySalary(){
+    
+}
+
+function dropDown(){
+    var mylist=document.getElementById("myList");
+    document.getElementById("favorite").value=mylist.options(mylist.selectedIndex).text;
+}
