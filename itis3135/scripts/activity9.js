9ee8a6726d3b20d7f0680e7834811264cceceec8
msolom15 <supersaiyen809@gmail.com>
2021-10-24 23:20:43 -0400

    f

---
+++ itis3135/scripts/activity9.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+   slider =  $('.slideshow').bxSlider();
+    });
+    
+  
+function image1(){
+       slider.goToSlide(0);
+
+}
+
+function image2(){
+       slider.goToSlide(1);
+
+}
+function image3(){
+       slider.goToSlide(2);
+
+}
+function image4(){
+       slider.goToSlide(3);
+
+}
+function image5(){
+       slider.goToSlide(4);
+
+}
+function image6(){
+       slider.goToSlide(5);
+
+}
