9802ff52d3dd6dd7de7b1be3a868b8dadfd1fabc
msolom15 <supersaiyen809@gmail.com>
2021-11-07 23:27:01 -0500

    ff

--- Project A/scripts/slideshow.js
+++ Project A/scripts/slideshow.js
@@ -1,24 +1,3 @@
-var i =0;
-  var images=[];
-  
-  
-  
-  images[0]= 'images/pic1.jpg';
-  images[1]= 'images/pic2.jpg';
-  images[2] = 'images/pic3.png';
-  
-  function changeImg(){
-    document.slideShow.src = images[i];
-    
-    if(i<images.length - 1){
-      i++
-    } else{
-      i=0;
-    }
-    window.onload=changeImg;
-    return false;
-  }
-  
-global.$("button").click(function(){
-  global.$("div").animate({left: '250px'});
-});
+$(document).ready(function(){
+   slider =  $('.slideshow').bxSlider();
+    });
