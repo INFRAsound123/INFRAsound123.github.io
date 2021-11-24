a6306c9c7f39ec2125d1682616fcdb8e4260b0bd
msolom15 <supersaiyen809@gmail.com>
2021-11-23 22:20:52 -0500

    3

--- itis3135/scripts/activity5script.js
+++ itis3135/scripts/activity5script.js
@@ -9,31 +9,39 @@ var h = (d.getHours()-12);
 var m = d.getMinutes();
 var s = d.getSeconds();
 
-document.write("Today is " + a + "/" + dt + "/" + y + ", and the current time is " + h + ":" + m + ":" + s);
-document.write("<br>"+"<br>"+"The Macho Yankee Salamander Welcomes you, " + user);
-document.write("<br>"+"<br>"+"We are glad you are doing "+ feeling);
+var x = ("Today is " + a + "/" + dt + "/" + y + ", and the current time is " + h + ":" + m + ":" + s);
+var b =("<br>"+"<br>"+"The Macho Yankee Salamander Welcomes you, " + user);
+var l =("<br>"+"<br>"+"We are glad you are doing "+ feeling);
+
+document.write("<font color=\"yellow\">" + x + "</font>");
+document.write("<font color=\"yellow\">" + b + "</font>");
+document.write("<font color=\"yellow\">" + l + "</font>");
 
 function button1() {
   var btn1 = document.createElement("button");
-  document.body.appendChild(btn1);
+  window.open("https://www.indeed.com/jobs?q=Internship&mna=5&aceid&gclid=CjwKCAiAv_KMBhAzEiwAs-rX1KhK9FgY-ROxaJgo5UAO9mmIudi1he_-9kzkS0bR8XennP8yXtUkohoCQncQAvD_BwE&vjk=9344c13d838569f8")
 }
 
 function button2() {
   var btn2 = document.createElement("button");
-  document.body.appendChild(btn2);
+  document.getElementById('pic').style.display='block';
 }
 
 function button3() {
   var btn3 = document.createElement("button");
-  document.body.appendChild(btn3);
+  document.getElementById("text").innerHTML = "Some ways to deal with heat is to use our fire-resistant paint as it contains properties that allows for the maximum heat absorbtion up to 100 degrees Farenheit and you can use our special drink which contains special minerals which allow your body to essenttially reflect heat and only keep the heat that you need"
 }
 
 function button4() {
   var btn4 = document.createElement("button");
-  document.body.appendChild(btn4);
+  var hours=prompt("Enter number of hours you would work in a day")
+  var days=prompt("Enter number of days in a week you would work")
+  var final= ((16.50*hours)*days)*4
+  alert("You would be making rougly $"+final+" in a month")
 }
 
 function button5() {
   var btn5 = document.createElement("button");
-  document.body.appendChild(btn5);
+  var x=prompt("Enter your email to recieve more information on our company")
+  alert("Email Received")
 }
