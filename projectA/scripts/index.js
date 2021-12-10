7db8c2867e4c29f14048065f98b55f3b201f323a
msolom15 <supersaiyen809@gmail.com>
2021-12-10 14:11:42 -0500

    f

---
+++ projectA/scripts/index.js
@@ -0,0 +1,27 @@
+function ajax() {
+    const doc = new XMLHttpRequest();
+    doc.onload = function() {
+      developers(this);
+    }
+    doc.open("GET", "people.xml");
+    doc.send();
+  }
+  function developers(xml) {
+    const xmlDoc = xml.responseXML;
+    const x = xmlDoc.getElementsByTagName("table");
+    var table="<tr><th>Name</th><th>Social</th></tr>";
+    for (var i = 0; i <x.length; i++) { 
+      table += "<tr><td>" +
+      x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("social")[0].childNodes[0].nodeValue +
+      "</td></tr>";
+    }
+    document.getElementById("people").innerHTML = table;
+  }
+  
+
+$('raptor').click(function(){
+    window.location='https://raptor.martincarlisle.com' + this.id;
+ })
+
---
+++ projectA/scripts/python.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $.ajax({
+        url: 'programmers.json',
+        data:'data',
+         beforeSend: function() {
+            $("#programmer").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#programmer").html("");
+            
+            $.getJSON("programmers.json", function(data){ 
+        $.each(data, function() { 
+            $.each(this, function(key, value) { 
+                $("#programmer").append( 
+                    "<h2>Name"+value.follow+"</h2>"+"<img src="+value.image+">"
+                     
+                     
+                    
+                    )
+            })
+        })
+            })
+        }
+            
+    })
+})
